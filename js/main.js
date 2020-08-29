54fa3ba316804da31a9985bc9848f8fb6e0242f0
Mohamed Ammar <ammarmohamed074@gmail.com>
2020-08-29 06:10:38 +0200

    Adding Project Files

---
+++ js/main.js
@@ -0,0 +1,269 @@
+$(window).on('load', function() {
+
+    $(".loading-overlay img").fadeOut(500,
+        function() {
+            $(this).parent().fadeOut(500);
+            $("body").removeClass("overflow")
+        });
+})
+
+var wow = new WOW({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null, // optional scroll container selector, otherwise use window,
+    resetAnimation: true, // reset animation on end (default is true)
+});
+wow.init();
+
+$(document).ready(function() {
+
+    // This is to Open Language menu in large Screens
+    $(".page-language .lang .lang-name").click(function() {
+        $(".lang-back").fadeIn(300);
+    });
+
+    $(".page-language .lang .lang-name").click(function() {
+        $(".lang .lang-list").slideDown(300);
+    });
+
+    $(".lang-back").click(function() {
+        $(".lang-back").fadeOut(300);
+    });
+
+    $(".lang-back").click(function() {
+        $(".lang .lang-list").slideUp(300);
+    });
+
+
+
+    // This is to Open Configuration menu in 
+    $(".user-terms .config .config-link").click(function() {
+        $(".config .config-link span").removeClass("open-config")
+        $(".config .config-link span").addClass("close-config")
+        $(".overlay-box").fadeIn(300);
+    });
+
+    $(".user-terms .config .config-link").click(function() {
+        $(".config .config-list").slideDown(300);
+    });
+
+    $(".overlay-box").click(function() {
+        $(".overlay-box").fadeOut(300);
+    });
+
+    $(".overlay-box").click(function() {
+        $(".config .config-link span").addClass("open-config")
+        $(".config .config-link span").removeClass("close-config")
+        $(".config .config-list").slideUp(300);
+    });
+
+
+    //This is to Open Search Box in small screens
+    $(".user-terms .search .search-link").click(function() {
+        $(".overlay-box2").fadeIn(300);
+    });
+
+    $(".user-terms .search .search-link").click(function() {
+        $(".search .search-link span").removeClass("open-search")
+        $(".search .search-link span").addClass("close-search")
+        $(".search-box").slideDown(300);
+    });
+
+    $(".overlay-box2").click(function() {
+        $(".overlay-box2").fadeOut(300);
+    });
+
+    $(".overlay-box2").click(function() {
+        $(".search .search-link span").addClass("open-search")
+        $(".search .search-link span").removeClass("close-search")
+        $(".search-box").slideUp(300);
+    });
+
+
+
+    $('.main-slider .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        margin: 50,
+        rtl: true,
+        navText: ["<span class='lnr lnr-chevron-right'></span>", "<span class='lnr lnr-chevron-left'></span>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                dots: true,
+                loop: true
+            },
+            768: {
+                items: 1,
+                nav: false,
+                dots: true,
+                loop: true
+            },
+            992: {
+                items: 1,
+                nav: true,
+                dots: true,
+                loop: true
+            },
+            1200: {
+                items: 1,
+                nav: true,
+                dots: true,
+                loop: true
+            }
+        }
+    })
+
+
+    $('.customers .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        margin: 30,
+        rtl: true,
+        navText: ["<span class='lnr lnr-chevron-right'></span>", "<span class='lnr lnr-chevron-left'></span>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false,
+                loop: true
+            },
+            480: {
+                items: 1,
+                dots: true,
+                nav: false,
+                loop: true
+            },
+            720: {
+                items: 2,
+                dots: true,
+                nav: false,
+                loop: true
+            },
+            992: {
+                items: 2,
+                nav: true,
+                dots: false,
+                loop: true
+            },
+            1200: {
+                items: 2,
+                nav: true,
+                dots: false,
+                loop: true
+            }
+        }
+    });
+
+
+    if ($(window).width() <= 991) {
+        $(".products-box").addClass("owl-carousel owl-theme");
+        $(".products-box .product").removeClass("owl-carousel");
+        $('.offers .owl-carousel').owlCarousel({
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            margin: 30,
+            rtl: true,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false,
+                    loop: true
+                },
+                500: {
+                    items: 2,
+                    nav: false,
+                    loop: true
+                }
+            }
+        })
+    } else {
+        $(".products-box").removeClass("owl-carousel");
+        $(".products-box .product").removeClass("owl-carousel");
+    };
+
+
+    if ($(window).width() <= 991) {
+        $(".features-box").addClass("owl-carousel owl-theme");
+        $(".features-box .feature").removeClass("owl-carousel");
+        $('.features .owl-carousel').owlCarousel({
+            loop: false,
+            margin: 10,
+            rtl: true,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false,
+                    loop: false
+                },
+                480: {
+                    items: 2,
+                    nav: false,
+                    loop: false
+                }
+            }
+        })
+    } else {
+        $(".features-box").removeClass("owl-carousel");
+        $(".features-box .feature").removeClass("owl-carousel");
+    };
+
+
+    if ($(window).width() <= 991) {
+        $('.collapse[data-parent="#parent"]').on('show.bs.collapse', function() {
+            $('.collapse[data-parent="#parent"]').not(this).collapse('hide');
+        });
+    } else {
+        $(".slide-list").addClass("in")
+    }
+
+    //This is to Open Side Menu in Small Screens
+    $(".menu-button .menu-link").click(function() {
+        $("body").addClass("overflow")
+        $(".side-menu").addClass("overflow");
+        $(".overlay-box").fadeIn(500);
+    });
+
+    $(".menu-button .menu-link").click(function() {
+        $(".side-menu").addClass("overflow");
+        $(".side-menu").addClass("menu-open");
+        $(".overlay-box").fadeIn(500);
+    });
+
+    $(".close,.overlay-box").click(function() {
+        $("body").removeClass("overflow")
+        $(".side-menu").removeClass("overflow");
+        $(".overlay-box").fadeOut(500);
+    });
+
+    $(".close,.overlay-box").click(function() {
+        $(".side-menu").removeClass("overflow");
+        $(".side-menu").removeClass("menu-open");
+        $(".back-overlay").fadeOut(500);
+    });
+
+
+    //This is to Open user-config & language Menus in Small Screens Side Menu
+    $(".account p").click(function() {
+        $(".account ul").slideToggle(600);
+    });
+
+    $(".language p").click(function() {
+        $(".language ul").slideToggle(600);
+    });
+});
